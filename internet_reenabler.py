2ada070271c5e3f4db40f76b107e43e5f1453867
lobstarr <anton93@gmail.com>
2021-12-30 14:23:23 +0300

    Temporary save

--- internet_reenabler.py
+++ internet_reenabler.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import subprocess
 
@@ -53,25 +56,51 @@ def ping_ip(address):
 def fix_router_settings():
     chrome_options = Options()
     # chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--disable-gpu")
     # chrome_options.add_argument("--no-sandbox") # linux only
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     # chrome_options.headless = True # also works
-    driver = webdriver.Chrome(options=chrome_options)
-    start_url = "https://duckgo.com"
-    driver.get(start_url)
-    print(driver.page_source.encode("utf-8"))
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager(cache_valid_range=365)),
+    #                           options=chrome_options)
+    chromedriver_path = 'C:\\ESD\\chromedriver.exe'
+    if not chromedriver_path:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=chrome_options)
+    else:
+        driver = webdriver.Chrome(service=Service(chromedriver_path),
+                                  options=chrome_options)
+    router_address = "http://192.168.1.1"
+    driver.get(router_address)
+    assert "http://192.168.1.1/" == driver.current_url
+
+    login_field = driver.find_element(By.ID, 'Frm_Username')
+    login_field.clear()
+    login_field.send_keys("mgts")
+    password_field = driver.find_element(By.ID, 'Frm_Password')
+    password_field.clear()
+    password_field.send_keys("mtsoao")
+    submit_password = driver.find_element(By.ID, 'LoginId')
+    submit_password.click()
+    driver.refresh()
+    # network_section = driver.find_element(By.XPATH,
+    #                                       '//*[@id="menu0"]/table/tbody/tr/td/table/tbody/tr[16]')
+    network_section = driver.find_element(By.ID, 'DhcpMode0')
+    network_section.click()
+
+    pass
+
+    print(driver.page_source)
     driver.quit()
 
 
 if __name__ == '__main__':
-    ping_test_ips = ['8.8.8.8', '77.88.8.8', '192.168.166.1']
-    ping_result = False
-    for ip in ping_test_ips:
-        for i in range(3):
-            if ping_ip(ip):
-                ping_result = True
-
-    if check_route_1_1(ping_test_ips[0]):
-        fix_router_settings()
-
+    # ping_test_ips = ['8.8.8.8', '77.88.8.8', '192.168.166.1']
+    # ping_result = False
+    # for ip in ping_test_ips:
+    #     for i in range(3):
+    #         if ping_ip(ip):
+    #             ping_result = True
+    #
+    # if check_route_1_1(ping_test_ips[0]):
+    #     fix_router_settings()
+    fix_router_settings()
